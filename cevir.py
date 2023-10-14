2a4ed5f411d37e0b69d4c30dcad651bb3c3ee1ab
Abdurrahim <abdurahim.abdo.zer@gmail.com>
2023-10-14 13:11:13 +0300

    Create cevir.py

---
+++ cevir.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+def lower(string):
+	liste = str.maketrans("ABCÇDEFGĞHIİJKLMNOÖPQRSŞTUÜVXWYZ0123456789","abcçdefgğhıijklmnoöpqrsştuüvxwyz0123456789")
+	return string.translate(liste)
+
+def upper(string):
+	liste = str.maketrans("abcçdefgğhıijklmnoöpqrsştuüvxwyz0123456789","ABCÇDEFGĞHIİJKLMNOÖPQRSŞTUÜVXWYZ0123456789")
+	return string.translate(liste)
